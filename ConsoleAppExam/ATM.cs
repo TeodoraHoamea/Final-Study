9f82be75a3f4042a9da691a1bcab784e04da77bc
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 21:30:09 +0200

    final app

--- ConsoleAppExam/ATM.cs
+++ ConsoleAppExam/ATM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleAppExam
@@ -7,6 +8,8 @@ namespace ConsoleAppExam
     public class ATM
     {
         private static ATM _instance;
+        private Card _card;
+        private Account _account;
         private ATM()
         {
             Console.WriteLine("New Instance of ATM");
@@ -22,5 +25,25 @@ namespace ConsoleAppExam
                 return _instance;
             }
         }
+
+        public void Insert(Card card)
+        {
+            _card = card;
+        }
+
+        public void SelectAccount(Account account)
+        {
+            var acc = _card.Accounts.Single(a => a == account);
+            _account = account;
+        }
+
+        public void withdraw(decimal ammount)
+        {
+            _account.withdraw(ammount);
+            Console.WriteLine($"You withdraw {ammount} from this account");
+            Console.WriteLine($"Remaining {_account.Balance} ");
+
+        }
+        public void Eject(Card card) { }
     }
 }
