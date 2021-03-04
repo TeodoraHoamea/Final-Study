3d4aa66d81b3cc5c452c27f185040619c22ecf90
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 20:46:46 +0200

    Added class for Savings Account

---
+++ ConsoleAppExam/SavingsAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppExam
+{
+    public class SavingsAccount : Account
+    {
+        protected override decimal CalculateWithDraw(decimal ammount)
+        {
+            return ammount * 0.5m / 100m;
+        }
+    }
+}
