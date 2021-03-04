d02dfab4c2fb60628cc1019cda18bf109347ff8d
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 20:32:08 +0200

    Added console app to the existing solution

---
+++ ConsoleAppExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
