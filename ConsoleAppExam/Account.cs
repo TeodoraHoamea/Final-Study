1972fb8480125dce7c3bd5e43866b893f3275668
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 20:43:05 +0200

    Created Account class.

---
+++ ConsoleAppExam/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppExam
+{
+    public abstract class Account
+    {
+        public decimal Balance { get; private set; }
+
+        public void Deposite(decimal ammount)
+        {
+            this.Balance += ammount;
+        }
+
+        public decimal withdraw(decimal ammount)
+        {
+            var fee = CalculateWithDraw(ammount);
+            ammount = ammount + fee;
+            if(Balance < ammount)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                this.Balance -= ammount;
+            }
+            return ammount;
+        }
+
+        protected abstract decimal CalculateWithDraw(decimal ammount);
+    }
+}
