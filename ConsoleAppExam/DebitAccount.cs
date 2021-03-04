b7177eeb01697332c62c535570c15bba7229f21b
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 20:47:58 +0200

    Added DebitAccount class

---
+++ ConsoleAppExam/DebitAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppExam
+{
+    public class DebitAccount : Account
+    {
+        protected override decimal CalculateWithDraw(decimal ammount)
+        {
+            return 0m;
+        }
+    }
+}
